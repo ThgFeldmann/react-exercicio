60e9cc58bf1320a6eeb2d8e9a5939649d0203953
Thiago Feldmann <thiagofeldmann06@gmail.com>
2024-07-15 13:57:02 -0300

    imc exibido

--- Form-imc/src/App.jsx
+++ Form-imc/src/App.jsx
@@ -1,11 +1,43 @@
-import Index from './Index'
+import { useEffect, useState } from "react";
 
 function App() {
+
+  let [altura, setAltura] = useState('');
+  let [peso, setPeso] = useState('');
+
+  var imc;
+  
+  const calculo = () => { // arrow function q calcula o imc
+      imc = Math.round(peso / (altura * altura) * 100) / 100;
+  }
+
+  function validaValores() {
+    if (altura, peso == ('')) {
+      imc = 0;
+    } else {
+      calculo();
+    }
+    return (
+      <h3>Seu IMC é: {imc}</h3>
+    )
+  }
+
   return(
-    <>
-      <Index />
-    </>
+    <div class="container">
+      <h1>Calculadora de IMC</h1>
+        <form>
+          <input type="number" placeholder="Digite sua altura" required onChange={evento => setAltura(evento.target.value)} />
+          <input type="number" placeholder="Digite seu peso" required  onChange={evento => setPeso(evento.target.value)}/>
+        </form>
+        {validaValores()}
+    </div>
   )
 }
 
 export default App
+
+// p / h^2 | calculo imc
+
+// mostrar a tabela de imc | objetivo atual
+
+// https://www.programasaudefacil.com.br/calculadora-de-imc | link imc calculator example
