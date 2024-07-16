513b611f4b1d4b36b2d3ec56afd30e8d65e006b1
Thiago Feldmann <thiagofeldmann06@gmail.com>
2024-07-16 15:24:46 -0300

    tabela adicionada
    - tabela inserida (n reage)
    - inputs desalinhados
    - borda do imc grande demais

--- Form-imc/src/App.jsx
+++ Form-imc/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 function App() {
 
@@ -18,18 +18,91 @@ function App() {
       calculo();
     }
     return (
-      <h3>Seu IMC é: {imc}</h3>
+      <h3 className="text-center mb-4 border border-success">Seu IMC é: {imc}</h3>
     )
   }
 
   return(
     <div class="container">
-      <h1>Calculadora de IMC</h1>
-        <form>
-          <input type="number" placeholder="Digite sua altura" required onChange={evento => setAltura(evento.target.value)} />
+      <h1 className="text-center">Calculadora de IMC</h1>
+        <form class='p-4'>
+          <input className="me-3" type="number" placeholder="Digite sua altura" required onChange={evento => setAltura(evento.target.value)} />
           <input type="number" placeholder="Digite seu peso" required  onChange={evento => setPeso(evento.target.value)}/>
         </form>
         {validaValores()}
+        <div className="border rounded border-dark"> {/* tabela */}
+          <div className="row m-3 text-center border-bottom border-dark">
+            <h2>Tabela de interpretação do IMC</h2>
+          </div>
+          <div className="ms-5"> {/* colunas */}
+            <div className="row border-bottom"> {/* categorias */}
+              <div className="col-md-4">
+                <h4>IMC</h4>
+              </div>
+              <div className="col-md-4">
+                <h4>Classificação</h4>
+              </div>
+              <div className="col-md-4">
+                <h4>Grau de obesidade</h4>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-4">
+                <h5>Menor que 18,5</h5>
+              </div>
+              <div className="col-md-4">
+                <h5>Magreza</h5>
+              </div>
+              <div className="col-md-4">
+                <h5>0</h5>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-4">
+                <h5>Entre 18,5 e 24,9</h5>
+              </div>
+              <div className="col-md-4">
+                <h5>Normal</h5>
+              </div>
+              <div className="col-md-4">
+                <h5>0</h5>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-4">
+                <h5>Entre 25,0 e 29,9</h5>
+              </div>
+              <div className="col-md-4">
+                <h5>Sobrepeso</h5>
+              </div>
+              <div className="col-md-4">
+                <h5>I</h5>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-4">
+                <h5>Entre 30,0 e 39,9</h5>
+              </div>
+              <div className="col-md-4">
+                <h5>Obesidade</h5>
+              </div>
+              <div className="col-md-4">
+                <h5>II</h5>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-4">
+                <h5>Maior que 40,0</h5>
+              </div>
+              <div className="col-md-4">
+                <h5>Obesidade Grave</h5>
+              </div>
+              <div className="col-md-4">
+                <h5>III</h5>
+              </div>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
@@ -38,6 +111,6 @@ export default App
 
 // p / h^2 | calculo imc
 
-// mostrar a tabela de imc | objetivo atual
+// alinhar os inputs | objetivo atual
 
 // https://www.programasaudefacil.com.br/calculadora-de-imc | link imc calculator example
--- Form-imc/src/main.jsx
+++ Form-imc/src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"; // bootstrap css import
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
